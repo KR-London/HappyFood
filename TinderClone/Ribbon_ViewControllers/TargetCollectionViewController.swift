3ac3462678ff874dfe15250aa7885b0871d62588
Kate Roberts <ekaterina.roberts@gmail.com>
2019-01-08 09:38:08 +0000

    cell delete behaviour implemeneted

--- TinderClone/Ribbon_ViewControllers/TargetCollectionViewController.swift
+++ TinderClone/Ribbon_ViewControllers/TargetCollectionViewController.swift
@@ -112,7 +112,12 @@ class TargetCollectionViewController: UICollectionViewController, CommunicationC
         {
             if 2*indexPath.section + indexPath.row  < foodsTriedThisWeek.count
             {
-                cell.foodImage.image = UIImage(named: "Tick.jpeg")
+                
+                let tickedImage = UIImage(named: foodsTriedThisWeek[2*indexPath.section + indexPath.row].0!)
+                
+                cell.foodImage.image = tickedImage
+                
+                //"tick.jpg"
                 if longPressedEnabled   {
                     cell.startAnimate()
                 }else{
@@ -227,4 +232,23 @@ class TargetCollectionViewController: UICollectionViewController, CommunicationC
     
  
 
+}
+
+
+extension UIImage {
+    
+    func overlayWith(image: UIImage, posX: CGFloat, posY: CGFloat) -> UIImage {
+        let newWidth = size.width < posX + image.size.width ? posX + image.size.width : size.width
+        let newHeight = size.height < posY + image.size.height ? posY + image.size.height : size.height
+        let newSize = CGSize(width: newWidth, height: newHeight)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        draw(in: CGRect(origin: CGPoint.zero, size: size))
+        image.draw(in: CGRect(origin: CGPoint(x: posX, y: posY), size: image.size))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
 }
